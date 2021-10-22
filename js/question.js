86ca3e96d44dd6fe7da21bde001197a5fef3f251
dindaviera <90680400+dindaviera@users.noreply.github.com>
2021-10-22 16:39:59 +0700

    update

---
+++ js/question.js
@@ -0,0 +1,30 @@
+const quiz = [
+    {
+        q : "Dalam sebulan terakhir, seberapa sering Anda merasa kesal karena sesuatu yang terjadi secara tidak terduga?",
+        options : ["Tidak pernah", "Kadang-kadang", "Sering", "Sangat sering"],
+        answer :"0"
+    },
+    {
+        q : "Dalam sebulan terakhir, seberapa sering Anda merasa bahwa anda tidak dapat mengendalikan hal-hal yang penting dalam hidup Anda?",
+        options  : ["Tidak pernah", "Kadang-kadang", "Sering", "Sangat sering"],
+        answer :"1"
+    },
+    {
+        q : "Dalam sebulan terakhir, seberapa sering Anda merasa gelisah dan stress?",
+        options : ["Tidak pernah", "Kadang-kadang", "Sering", "Sangat sering"],
+        answer :"0"
+    },
+    {
+        q : "Dalam sebulan terakhir, seberapa sering Anda merasa yakin terhadap kemampuan Anda dalam menangani masalah pribadi?",
+        options : ["Tidak pernah", "Kadang-kadang", "Sering", "Sangat sering"],
+        answer :"2"
+    },
+    {
+        q : "Dalam sebulan terakhir, seberapa sering Anda merasa bahwa segala sesuatu berjalan sesuai keinginan Anda?",
+        options : ["Tidak pernah", "Kadang-kadang", "Sering", "Sangat sering"],
+        answer :"1"
+    }
+]
+
+
+
